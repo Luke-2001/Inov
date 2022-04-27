25352b776d9398b0b8f15964294efc8f43487994
Lucas Bassanesi Biscaia <lucasbassanesib@gmail.com>
2022-04-27 20:45:55 -0300

    27-04-2022

--- src/Main.java
+++ src/Main.java
@@ -5,14 +5,14 @@ public class Main {
     public static void main(String[] args) {
         Pessoa pessoa = new Pessoa();
 
-        Integer idade;
+        int idade;
         Scanner f = new Scanner(System.in);
 
         System.out.print("Informe o Nome do Aluno: ");
         pessoa.setNome(f.nextLine());
         System.out.println("Nome do Aluno é: " + pessoa.nome + ".\n");
 
-        System.out.print("Informe a sua Data de Nascimento: ");
+        System.out.print("Informe o seu Ano de Nascimento: ");
         pessoa.setDataNasc(f.nextInt());
 
         pessoa.setAno(2022);
